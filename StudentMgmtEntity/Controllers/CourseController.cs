e3ab1b5226c4e940010493aa98540e5dd8ab53e9
Basim Hilal <basimhilalofficial@gmail.com>
2025-04-01 12:04:33 +0530

    Added Course Controller

--- StudentMgmtEntity/Controllers/CourseController.cs
+++ StudentMgmtEntity/Controllers/CourseController.cs
@@ -16,9 +16,44 @@ namespace StudentMgmtEntity.Controllers
         }
 
         [HttpGet("{id}")]
-        public Task<ActionResult<Course>> getCourse(int id)
+        public async Task<ActionResult<Course>> getCourse(int id)
         {
+            var course = await _courseServices.GetOneCourse(id);
+            if (course == null) return NotFound();
+            return Ok(course);
 
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<IEnumerable<Course>>> getCourses()
+        {
+            var courses = await _courseServices.GetCourses();
+            if (courses == null) return NotFound();
+            return Ok(courses);
+        }
+
+        [HttpPost("Create")]
+        public async Task<ActionResult> createCourse(CourseDto request)
+        {
+            var created = await _courseServices.AddCourse(request);
+            if (created == null) return BadRequest();
+            return NoContent();
+        }
+
+        [HttpPut("Update")]
+        public async Task<ActionResult> updateCourse(int id, CourseDto request)
+        {
+            var updated = await _courseServices.UpdateCourse(id, request);
+            if (updated == null) return NotFound();
+            return NoContent();
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<ActionResult> deleteCourse(int id)
+        {
+            var deleted = await _courseServices.DeleteCourse(id);
+            if (deleted == null) return NotFound();
+            return NoContent();
         }
     }
 }
